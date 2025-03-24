89f2c00e408a6a57ad0219c76ba82075d9f10199
athavan710 <athavan.mahadevan@student.tdsb.on.ca>
2025-03-24 09:29:16 -0400

    Update PolynomialEvaluator.java

--- src/PolynomialEvaluator.java
+++ src/PolynomialEvaluator.java
@@ -6,9 +6,8 @@ public class PolynomialEvaluator {
         Scanner keyboard = new Scanner(System.in);
 
         System.out.println("Enter your polynomial in the form of f(x)=3x^3 -5x^2 +1x^4 +9x^6 +3.1x^1 +2");
-        String polynomial = keyboard.nextLine();
 
-        StringTokenizer st = new StringTokenizer(polynomial, " ");
+        StringTokenizer st = new StringTokenizer(keyboard.nextLine().replace("f(x)=", ""));
         System.out.println("Enter the value of x");
         double x = keyboard.nextDouble();
 
@@ -19,31 +18,24 @@ public class PolynomialEvaluator {
             int exponent = 0;
             double coefficient = 1;
 
-            int xIndex = currentTerm.indexOf('x');
-            int exponentIndex = currentTerm.indexOf('^');
-
-            if (xIndex >= 0) {
-                if (xIndex == 0) {
-                    coefficient = 1;
-                } else {
-                    coefficient = Double.parseDouble(currentTerm.substring(0, xIndex));
+            if (currentTerm.contains("x")) {
+                if (currentTerm.replaceAll("/+|-", "").indexOf("x") != 0) {
+                    coefficient = Double.parseDouble(currentTerm.substring(0, currentTerm.indexOf('x')));
                 }
-                if (exponentIndex >= 0) {
-                    exponent = Integer.parseInt(currentTerm.substring(exponentIndex + 1));
+                if (currentTerm.contains("^")) {
+                    exponent = Integer.parseInt(currentTerm.substring(currentTerm.indexOf('^') + 1));
                 } else {
                     exponent = 1;
-
                 }
 
             } else {
                 coefficient = Double.parseDouble(currentTerm);
-                exponent = 0;
 
             }
             result += coefficient * Math.pow(x, exponent);
 
         }
-        System.out.println("f" +"(" + x + ")" + " = " + result);
+        System.out.println("f" + "(" + x + ")" + " = " + result);
         keyboard.close();
     }
 }
